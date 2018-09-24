cd61eac84ff3213ca17fd991c5be7bf41203d943
André Schulz <anschu.git@posteo.de>
2018-09-24 21:19:29 +0200

    implemented folder renaming and git file removal

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-function set_id {
+function init_project {
 	IFS="." read -a ID_PARTS <<< "${1}"
 
 	sed -i -e "s/_APPLICATION_ID_/${1}/g" CMakeLists.txt
 	sed -i -e "s/_PROJECT_NAME_/${ID_PARTS[-1]}/g" CMakeLists.txt
 
+	CURRENT_DIR="$(echo "${PWD##*/}")"
+	mv ../$CURRENT_DIR ../${ID_PARTS[-1]}
+
+	rm -r .git*
 	echo "Project initialized as '${1}'"
 }
 
@@ -16,4 +20,6 @@ then
 	PROJECT_ID="de.anschu.GtkmmApplicationTemplate"
 fi
 
-set_id $PROJECT_ID
+init_project $PROJECT_ID
+
+
